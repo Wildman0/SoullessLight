4dc2402beff3b74c8f6db163e0dc5e22d22ccee8
ISAAC OLUYADI <IO190314@falmouth.ac.uk>
2019-01-22 10:40:44 +0000

    ISAAC
    added it to if you get near the ledge, the prompt appears

---
+++ Assets/UIAppear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppear : MonoBehaviour
+{
+    [SerializeField] private GameObject customImage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.SetActive(true);
+            customImage.GetComponent<Animator>().SetTrigger("Caution");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.GetComponent<Animator>().SetTrigger("NoCaution");
+        }
+    }
+}
