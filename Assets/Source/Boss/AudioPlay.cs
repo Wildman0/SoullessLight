657220564414d0f600d3a93be913cdda2408dcad
ISAAC OLUYADI <IO190314@falmouth.ac.uk>
2019-01-24 13:39:34 +0000

    audio change testing

---
+++ Assets/Source/Boss/AudioPlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+
+
+    public AudioSource audioData;
+    // Use this for initialization
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.Play();
+
+    }
+}
