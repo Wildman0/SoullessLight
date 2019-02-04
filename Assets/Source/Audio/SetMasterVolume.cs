488c056ec60d4df44756c9b7b033135030cc5180
c0ne1 <crice201@gmail.com>
2019-02-04 15:52:55 +0000

    Audio menu functionality added

--- Assets/Source/Audio/SetMasterVolume.cs
+++ Assets/Source/Audio/SetMasterVolume.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class SetVolume : MonoBehaviour
+public class SetMasterVolume : MonoBehaviour
 {
 
     public AudioMixer mixer;
+    public Slider slider;
 
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
+    }
     public void SetLevel(float sliderValue)
     {
         mixer.SetFloat("masterVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
     }
 }
