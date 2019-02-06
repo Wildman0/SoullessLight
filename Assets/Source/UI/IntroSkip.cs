6db499dddc2e27c8bbbba4d87a483987b45c1fd9
c0ne1 <crice201@gmail.com>
2019-02-06 15:56:07 +0000

    Main menu improvements

---
+++ Assets/Source/UI/IntroSkip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSkip : MonoBehaviour {
+
+    public GameObject introScreen;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            introScreen.SetActive(false);
+
+    }
+}
