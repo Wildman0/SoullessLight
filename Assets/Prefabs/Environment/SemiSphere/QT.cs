b52ab18b64f3817ecbb74b46bf2ac97f0fdaae23
PedrekW <46958431+PedrekW@users.noreply.github.com>
2019-02-11 11:01:14 +0000

    Added Water to the Fountain

---
+++ Assets/Prefabs/Environment/SemiSphere/QT.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QT : MonoBehaviour {
+
+    public float scrollX = 0.5f;
+    public float scrollY = 0.5f;
+
+
+    void Update()
+    {
+        float OffsetX = Time.time * scrollX;
+        float OffsetY = Time.time * scrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+
+    }
+}
