7a1eb25cb549d4e9067d139f62c48d4fcf226ddd
PedrekW <46958431+PedrekW@users.noreply.github.com>
2019-02-12 10:14:16 +0000

    Orbs have explosions now

--- Assets/Source/Boss/BossProjectile.cs
+++ Assets/Source/Boss/BossProjectile.cs
@@ -5,15 +5,30 @@ using Dreamteck.Splines;
 
 public class BossProjectile : MonoBehaviour
 {
+
+    public GameObject implosion;
     public SplineFollower follower;
     [SerializeField] private float projectileDamage = 0.1f;
-    
+
+    private void Start()
+    {
+        
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Implode();
             PlayerHealth.instance.TakeDamage(projectileDamage);
             Destroy(gameObject);
         }
+    }
+
+
+    void Implode()
+    {
+        GameObject implo = Instantiate(implosion, transform.position, Quaternion.identity);
+        implo.GetComponent<ParticleSystem>().Play();
     }
 }
