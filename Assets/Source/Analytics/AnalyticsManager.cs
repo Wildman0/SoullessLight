044cf2b7c6575e83979271f162366c4dcd62da6d
Kyle <kw190357@falmouth.ac.uk>
2019-02-27 14:44:09 +0000

    Created analytics manager

---
+++ Assets/Source/Analytics/AnalyticsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class AnalyticsManager : MonoBehaviour 
+{
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
