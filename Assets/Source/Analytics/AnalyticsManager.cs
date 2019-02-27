c87d07ae1451763cc8a0e8cf1782f41e83d30417
Kyle <kw190357@falmouth.ac.uk>
2019-02-27 15:05:59 +0000

    Added GameStatus loading

--- Assets/Source/Analytics/AnalyticsManager.cs
+++ Assets/Source/Analytics/AnalyticsManager.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Analytics;
 using System.Xml;
 using System.Xml.Serialization;
 
-public class AnalyticsManager : MonoBehaviour 
+public class AnalyticsManager : MonoBehaviour
 {
+	public GameStatus gameStatus;
+	
 	void Start () 
 	{
-		
+		LoadGameStatusFromFile();
 	}
-	
-	void Update () 
+
+	void LoadGameStatusFromFile()
 	{
-		
+		var path = Path.Combine(Application.dataPath, "Xml/GameStatus.xml");
+		using (var stream = new FileStream(path, FileMode.Open))
+		{
+			var serializer = new XmlSerializer(typeof(GameStatus));
+			gameStatus = (GameStatus) serializer.Deserialize(stream);
+		}
 	}
 }
