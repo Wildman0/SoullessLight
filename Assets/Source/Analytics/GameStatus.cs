aa4319af2c3bce06efd16bfe57af129ebcb5a0f7
Kyle <kw190357@falmouth.ac.uk>
2019-02-27 15:05:42 +0000

    Added GameStatus

---
+++ Assets/Source/Analytics/GameStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    public int versionRelease;
+    public int versionMajor;
+    public int versionMinor;
+    public int versionPatch;
+    
+    public string GetVersionNumber()
+    {
+        return versionRelease + "." + versionMajor + "." + versionMinor + "." + versionPatch;
+    }
+}
