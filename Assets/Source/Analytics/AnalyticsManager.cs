51ff3cf7057424ddca499141eee97a588a593771
Kyle <kw190357@falmouth.ac.uk>
2019-02-27 15:18:52 +0000

    Added placeholder Xml generation

--- Assets/Source/Analytics/AnalyticsManager.cs
+++ Assets/Source/Analytics/AnalyticsManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.Analytics;
 using System.Xml;
 using System.Xml.Serialization;
+using UnityEditor;
 
 public class AnalyticsManager : MonoBehaviour
 {
@@ -15,6 +16,26 @@ public class AnalyticsManager : MonoBehaviour
 		LoadGameStatusFromFile();
 	}
 
+	//Provides a template game status file
+	static GameStatus GenerateGameStatusFile()
+	{
+		return new GameStatus(new int[]{0, 5, 3, 1});
+	}
+
+	//To be used manually when you don't have an XML file to read from
+	[MenuItem("NDA/Analytics/Generate Game Status File")]
+	public static void SaveGeneratedGameStatusFile()
+	{
+		var path = Path.Combine(Application.dataPath, "Xml/GameStatus.xml");
+        
+		using (var stream = new FileStream(path, FileMode.Create))
+		{
+			var serializer = new XmlSerializer(typeof(GameStatus));
+
+			serializer.Serialize(stream, GenerateGameStatusFile());
+		}
+	}
+	
 	void LoadGameStatusFromFile()
 	{
 		var path = Path.Combine(Application.dataPath, "Xml/GameStatus.xml");
--- Assets/Source/Analytics/GameStatus.cs
+++ Assets/Source/Analytics/GameStatus.cs
@@ -4,12 +4,25 @@ using System.IO;
 using System.Xml.Serialization;
 using UnityEngine;
 
-public class GameStatus : MonoBehaviour
+public class GameStatus
 {
     public int versionRelease;
     public int versionMajor;
     public int versionMinor;
     public int versionPatch;
+
+    public GameStatus()
+    {
+        
+    }
+    
+    public GameStatus(int[] version)
+    {
+        versionRelease = version[0];
+        versionMajor = version[1];
+        versionMinor = version[2];
+        versionPatch = version[3];
+    }
     
     public string GetVersionNumber()
     {
