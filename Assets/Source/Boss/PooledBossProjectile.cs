4a296cb367dcbf7d3319fc7996b7b53a917152f9
pedrek <pedrek@live.nl>
2019-03-06 17:16:00 +0000

    Final Orb and Pooling Start

---
+++ Assets/Source/Boss/PooledBossProjectile.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+
+public class PooledBossProjectile : MonoBehaviour
+{
+   // public bool destroyAfterTime = false;
+    //public float destroyAfterXTime = 20f;
+    //public GameObject implosion;
+    //public GameObject enableMe;
+   // public GameObject spawn;
+    public SplineFollower follower;
+    [SerializeField] private float projectileDamage = 0.1f;
+
+    private void Start()
+    {
+     // needs interface due to pooling.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+
+
+    }  
+        
+        // if (other.tag == "Player")
+       // {
+        //    if (!PlayerHealth.instance.isInvincible)
+        //    {
+        //        Implode();
+      //          PlayerHealth.instance.TakeDamage(projectileDamage);
+       //         Destroy(gameObject);
+      //      }
+      //  }
+
+     //   if (other.CompareTag("ProjectileStopper"))
+      //  {
+      //      Implode();
+      //      Destroy(gameObject);
+       //     Debug.Log("Boom");
+        //}
+    
+
+
+  //  void Implode()
+ //   {
+  //      GameObject implo = Instantiate(implosion, transform.position, Quaternion.identity);
+  //      implo.GetComponent<ParticleSystem>().Play();
+   // }
+
+   // IEnumerator SpawnComplete()
+   // {
+   //     yield return new WaitForSeconds(1.5f);
+  //     enableMe.SetActive(true);
+        //Debug.Log("It's working");
+  //      Destroy(spawn);
+   // }
+
+ //   private void OnDestroy()
+ //   {
+ //       Implode();
+ //   }
+}
---
+++ Assets/SpawnerIntermission.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerIntermission : MonoBehaviour {
+
+    public string objectName;
+
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.instance;
+    }
+
+    private void FixedUpdate()
+    {
+        objectPooler.SpawnFromPool("Orb", transform.position, Quaternion.identity);
+    }
+
+}
