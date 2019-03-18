9a545f8ebe76464dd29a7798cee14040c03a2f66
PedrekW <46958431+PedrekW@users.noreply.github.com>
2019-03-18 13:17:18 +0000

    more changes to boss projectile

--- Assets/Source/Boss/BossProjectile.cs
+++ Assets/Source/Boss/BossProjectile.cs
@@ -10,23 +10,28 @@ public class BossProjectile : MonoBehaviour
     [SerializeField] private float projectileDamage = 0.1f;
     public static SplineComputer sc;
     private SplineFollower follower;
+    [SerializeField] private Animator anim;
 
     private void Start()
     {
-        sc = spawner.GetComponent<SplineComputer>();
-        follower = GetComponent<SplineFollower>();
-        follower.computer = sc; 
+       sc = spawner.GetComponent<SplineComputer>();
+       follower = GetComponent<SplineFollower>();
+       follower.computer = sc;
     }
 
     private void OnEnable()
     {
+        transform.localScale = new Vector3(0,0,0);
         Invoke("Destroy", destroyAfter);
+        anim.SetTrigger("StartSpawn");
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            Destroy();
+
             if (!PlayerHealth.instance.isInvincible)
             {
                 PlayerHealth.instance.TakeDamage(projectileDamage);
@@ -34,7 +39,7 @@ public class BossProjectile : MonoBehaviour
         }
     }
 
-    private void Destroy()
+    public void Destroy()
     {
         gameObject.SetActive(false);
         follower.Restart(0);
