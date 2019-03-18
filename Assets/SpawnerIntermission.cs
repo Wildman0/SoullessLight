9b6788874223eacbab7d8ad5f0795e31b4f28a99
PedrekW <46958431+PedrekW@users.noreply.github.com>
2019-03-18 10:27:12 +0000

    ObjectPooling finsihed up

--- Assets/SpawnerIntermission.cs
+++ Assets/SpawnerIntermission.cs
@@ -5,22 +5,25 @@ using Dreamteck.Splines;
 
 public class SpawnerIntermission : MonoBehaviour {
 
+    public bool infiniteSpawn = false;
+
+    public int pooledAmount = 20;
+
+    public float speed = 5f;
+    public float maxSpawnAmount = 10f;
+    private float spawnAmount = 0f;
     public float fireTime = 1;
     public float fireDelay = 1;
+
     public GameObject toSpawn;
-    public int pooledAmount = 20;
     List<GameObject> orbs;
-    public float speed = 5f;
 
-
+    // sets the pool size and the object then creates pool. then starts firing whilst setting span delay and time.
     private void Start()
     {
         SplineFollower follower = toSpawn.GetComponent<SplineFollower>();
         follower.followSpeed = speed;
-
-        
-
-
+      
         orbs = new List<GameObject>();
         for (int i = 0; i < pooledAmount; i++)
         {
@@ -33,8 +36,21 @@ public class SpawnerIntermission : MonoBehaviour {
 
     }
 
+    // removes orb from pool and disables it when maxSpawnAmount reached.
     void Fire()
     {
+        if (!infiniteSpawn)
+        {
+
+            if (spawnAmount == maxSpawnAmount)
+            {
+                CancelInvoke("Fire");
+            }
+            else
+            {
+                spawnAmount += 1;
+            }
+        }
         for (int i = 0; i < orbs.Count; i++)
         {
             if (!orbs[i].activeInHierarchy)
@@ -43,7 +59,7 @@ public class SpawnerIntermission : MonoBehaviour {
                 orbs[i].transform.rotation = transform.rotation;
                 orbs[i].SetActive(true);
                 break;
-            }
+            }                
         }
     }
 
