db789fccd0b4d84a203f29583da82b44fd2ab2a3
Kyle <kw190357@falmouth.ac.uk>
2019-03-28 14:29:20 +0000

    Added MusicController

--- Assets/Source/Audio/MusicController.cs
+++ Assets/Source/Audio/MusicController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMOD.Studio;
 
-public class MusicController : MonoBehaviour {
+public class MusicController : MonoBehaviour
+{
+	private EventInstance bossBattleMusic;
+	private ParameterInstance bossBattleStage;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Awake()
+	{
+		bossBattleMusic = FMODUnity.RuntimeManager.CreateInstance("event:/MainTheme");
+		bossBattleMusic.start();
+		bossBattleMusic.getParameter("bossBattleStage", out bossBattleStage);
+		Debug.Log(bossBattleStage);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void SetBossStageParameter(int i)
+	{
+		bossBattleMusic.setParameterValue("bossBattleStage", i);
 	}
 }
