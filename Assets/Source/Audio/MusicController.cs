5e4ac0ac794df99197f889b83a1080290b1bd333
Kyle <kw190357@falmouth.ac.uk>
2019-03-28 15:18:34 +0000

    Created trigger for boss fight beginning

--- Assets/Source/Audio/MusicController.cs
+++ Assets/Source/Audio/MusicController.cs
@@ -5,11 +5,17 @@ using FMOD.Studio;
 
 public class MusicController : MonoBehaviour
 {
+	public static MusicController instance;
 	private EventInstance bossBattleMusic;
 	private ParameterInstance bossBattleStage;
 
 	void Awake()
 	{
+		if (!instance)
+			instance = this;
+		else
+			Debug.LogError("More than one instance of MusicController in the scene");
+		
 		bossBattleMusic = FMODUnity.RuntimeManager.CreateInstance("event:/MainTheme");
 		bossBattleMusic.start();
 		bossBattleMusic.getParameter("bossBattleStage", out bossBattleStage);
---
+++ Assets/Source/Audio/SetBossStageCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBossStageCollider : MonoBehaviour 
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			MusicController.instance.SetBossStageParameter(1);
+		}
+	}
+}
