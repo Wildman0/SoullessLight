146f9ba050e651cd0973634288ea7a4b86ef2f33
PedrekW <46958431+PedrekW@users.noreply.github.com>
2019-04-03 14:05:18 +0100

    Tut Changes and start of functionality

---
+++ Assets/HurtBoxTutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtBoxTutorial : MonoBehaviour {
+
+    public GameObject me;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+                PlayerHealth.instance.TakeDamage(0.3f);
+                gameObject.active = false;
+        }
+    }
+
+
+}
