ae23c4a12ca81ec3ef82b35a90ffea0743b3cba0
ISAAC OLUYADI <IO190314@falmouth.ac.uk>
2019-04-09 10:58:14 +0100

    door function and optimisation

--- Assets/Source/Anim/DoorTrigger.cs
+++ Assets/Source/Anim/DoorTrigger.cs
@@ -6,12 +6,21 @@ public class DoorTrigger : MonoBehaviour
 {
     private bool doorIsOpen;
     [SerializeField] private Animator doorAnimator;
+    [SerializeField] private GameObject DoorBlocker;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             doorAnimator.SetTrigger("isOpen");
+            StartCoroutine(Door());
+            
         }
+    }
+
+    private IEnumerator Door()
+    {
+        yield return new WaitForSeconds(3);
+        DoorBlocker.SetActive(false);
     }
 }
