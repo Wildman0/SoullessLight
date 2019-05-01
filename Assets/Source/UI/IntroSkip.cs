9e776aff50ca77487dc481de44d5c147296a088c
ISAAC OLUYADI <IO190314@falmouth.ac.uk>
2019-05-01 09:55:48 +0100

    main menu audio fix

--- Assets/Source/UI/IntroSkip.cs
+++ Assets/Source/UI/IntroSkip.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class IntroSkip : MonoBehaviour {
 
     public GameObject introScreen;
+    public AudioSource mainTheme;
 	
 	// Update is called once per frame
 	void Update () {
 
         if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainTheme.Play();
             introScreen.SetActive(false);
+        }
+           
+            
 
         if (Input.GetKeyDown(KeyCode.JoystickButton7))
+        {
+            mainTheme.Play();
             introScreen.SetActive(false);
+        }
+           
+            
     }
 }
